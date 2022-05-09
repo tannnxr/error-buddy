f55baa4937993b5c7f8f7615557a8e289f28bab7
Tanner <104153588+tannnxr@users.noreply.github.com>
2022-05-09 07:24:36 -0400

    Create error-buddy.lua

---
+++ error-buddy.lua
@@ -0,0 +1,25 @@
+--[[
+
+	* Name: Script Checker (Beta)
+	* Description: Checks your scripts for errors on run time.  This will help ease debugging.
+	* Author: Tanner (@tannnnxr)
+	* Version: 1.0.0
+
+]] --
+
+
+
+
+local PLUGIN: Plugin = plugin;
+local TOOLBAR: PluginToolbar = PLUGIN:CreateToolbar("Script Checker");
+local PLUGIN_TOOLBAR_BUTTON = TOOLBAR:CreateButton("", "Opens the UI to check and monitor script errors", "", "Open UI")
+
+
+-- Initiate the plugin
+do
+	local PLUGIN: Plugin = plugin;
+	local TOOLBAR: PluginToolbar = PLUGIN:CreateToolbar("Script Checker");
+	local PLUGIN_TOOLBAR_BUTTON = TOOLBAR:CreateButton("", "Opens the UI to check and monitor script errors", "", "Open UI")
+	
+	
+end
